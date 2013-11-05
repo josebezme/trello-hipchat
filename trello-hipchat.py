726f3933f17f507c180d174c704f555d0533f30a
Jose Rodriguez <jose@meetup.com>
2013-11-05 10:29:11 -0500

    Added create card.

--- trello-hipchat.py
+++ trello-hipchat.py
@@ -174,6 +174,19 @@ def notify(board_id, list_names, room_id):
                 msg(room_id, "%s completed checklist item \"%s\" in card <a href=\"%s\">%s</a>" %
                     (author, ESC(A["data"]["checkItem"]["name"]), card_url, card_name))
 
+        elif A["type"] == "createCard":
+            card_id_short = A["data"]["card"]["idShort"]
+            card_id = A["data"]["card"]["id"]
+            card_url = "https://trello.com/card/%s/%s/%s" % (card_id, board_id, card_id_short)
+            card_name = ESC(card_name_strip(A["data"]["card"]["name"]))
+            list_name = ESC(A["data"]["list"]["name"])
+            author = ESC(A["memberCreator"]["fullName"])
+            if not card_in_lists(list_name, list_names):
+                continue
+
+            msg(room_id, "%s created card <a href=\"%s\">%s</a> in list \"%s\"" % (
+                    author, card_url, card_name, list_name))
+
     LAST_ID = max(LAST_ID, long(A["id"], 16))
 
 
